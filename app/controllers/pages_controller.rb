879bb14f98fb936eb850e83f57915e095daecaca
Joel Jackson <jackson.joel@gmail.com>
2014-11-08 10:57:41 -0800

    NOW only my friends can get on

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
-  http_basic_authenticate_with name: "joel", password: "joelisthebest"
+  before_filter :authenticate!
 
   def super_secret
+  end
+
+  private
+  def authenticate!
+    users = {
+      joel: 'awesome',
+      justin: 'sortofawesome',
+      jonny: 'finehesallowedontoo'
+    }
+
+    head :unauthorized unless params[:username].present? && params[:password].present? && users[params[:username].to_sym] == params[:password]
   end
 end
